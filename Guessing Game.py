b09381c77a3b7f59eac443d169334027ca66c14a
Rheena Catherine <rheenahmacharia95@gmail.com>
2021-03-26 16:19:17 +0300

    Add files via upload

---
+++ Guessing Game.py
@@ -0,0 +1,49 @@
+#Number guessing game
+
+import random
+attempts_list = []
+def show_score():
+    if len(attempts_list) <= 0:
+        print('There is currently no high score, it is yours for the taking!')
+    else:
+        print('The current high score is {} attempts'.format(min(attempts_list)))
+
+def start_game():
+    random_number = int(random.randint(1, 10))
+    print('Hello there! Welcome to the GUESSING GAME!')
+    player_name = input('What is your name? ')
+    wanna_play = input('Hi, {}, would you like to play the guessing game? (Enter Yes/No) '.format(player_name))
+
+#To show the scores
+    attempts = 0
+    show_score()
+    while wanna_play.lower() == "yes":
+            try:
+                guess = input('Pick a number between 1 and 10')
+                if int(guess) < 1 or int(guess) > 10:
+                    raise ValueError('Please guess a number within the given range')
+                if int(guess) == random_number:
+                    print('Nice! You got it!')
+                    attempts += 1
+                    attempts_list.append(attempts)
+                    print('It took you {} attempts'.format(attempts))
+                    play_again = input('Would you like to play again? (Enter Yes/No) ')
+                    attempts = 0
+                    show_score()
+                    random_number = int(random.randint(1, 10))
+                    if play_again.lower() == 'no':
+                        print('That is cool, have a good one!')
+                        break
+                elif int(guess) > random_number:
+                    print('Its lower')
+                    attempts += 1
+                elif int(guess) < random_number:
+                    print('Its higher')
+                    attempts += 1
+            except ValueError as err:
+                print('Oh no!, that is not a valid value. Try again...')
+                print('({})'.format(err))
+    else:
+        print('That is cool, have a good one!')
+if __name__ == '__main__':
+    start_game()
